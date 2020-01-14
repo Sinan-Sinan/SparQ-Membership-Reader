3b738b058745ba65f211b740c7343ab26a8d2f48
Sinan-Sinan <57467731+Sinan-Sinan@users.noreply.github.com>
2020-01-13 21:50:38 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,29 +1,34 @@
 ﻿<?php
-	session_start();
-	$cardString = $_SESSION['cardString'];
-    if(isset($_REQUEST['cardNum'])){
-		$cardString = htmlentities($_REQUEST['cardNum']);
-    }
+	session_start();//Starts Session to collect transfer variable values beetween tabs
+
+	$cardString = $_SESSION['cardString'];//Gets the Card Number in String Format from session
+	if(isset($_REQUEST['cardNum'])){//Request for Card Number
+		$cardString = htmlentities($_REQUEST['cardNum']);//Makes sures no injection 
+	}
 	else{
 		$cardString = '';
 	}
-	$cardNum = (int)$cardString;
 
-	if($cardNum > 8){
+	$cardNum = (int)$cardString;//Parses String
+
+	if($cardNum > 8){//Student Numbers are less than 8
 		$cardString = substr($cardNum, 0, 8);
 	}
-	$cardNum = (int)$cardString;
-	$_SESSION['cardString'] =  $cardString;
 
-	$spreadsheet_url="https://docs.google.com/spreadsheets/d/e/2PACX-1vQP6KOKJhG_39-3UNXFkCMjsKSr3gMGU_Rr3hp1mqoz5BXTEAwXwmqzAEKMrz8Cn_6DN4zfE5q_d43C/pub?gid=0&single=true&output=csv";
+	$cardNum = (int)$cardString;//I dont why this is here lol
+	$_SESSION['cardString'] =  $cardString;//Session to store Card string
 
-	if(!ini_set('default_socket_timeout', 10)) echo "<!-- unable to change socket timeout -->";
+	$spreadsheet_url="URL";//Spreadsheet URL (Use google spreadsheets)
 
+	if(!ini_set('default_socket_timeout', 10)) echo "<!-- unable to change socket timeout -->";//Gets the spreadsheet
+
+	//Variables to store data
 	$valid;
 	$cardNumber;
 	$membership;
 	$training;
 
+	//Gets the Data and stores them
 	if (($handle = fopen($spreadsheet_url, "r")) !== FALSE) {
 		while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
 			if($data[2] == $cardString){
@@ -40,21 +45,22 @@
 		print("ERROR");	
 	}
 
-	$colour;
+	$colour;//Variable to display colours
 
-	if(strcmp($valid,'VALID') == 0){
+	if(strcmp($valid,'VALID') == 0){//Makes sure that the Member is valid
 		$colour = 'green';
-	}elseif(strcmp($valid,'INVALID') == 0){
+	}elseif(strcmp($valid,'INVALID') == 0){//If Member didnt pay or didnt do training they are invalid
 		$colour = 'red';
-	}else{
+	}else{//If the number is not found
 		$colour = 'white';
 		$valid = 'NOT FOUND';
 	}
 
-	if(!(empty($cardNumber))){
+	if(!(empty($cardNumber))){//Refreshes the Tab after 1 sec unless if member is not found
 		header("refresh:1;url=index.php");
 	}
 
+	//Sends data to session (don't really need it)
 	$_SESSION['colour'] = $colour;
 	$_SESSION['valid'] =  $valid;
 	$_SESSION['cardNumber'] = $cardNumber;
@@ -70,7 +76,7 @@
     <title>SparQ Entry Reader</title>
 	<link rel="stylesheet" type="text/css" href="style/style.css">
 </head>
-<?php echo '<body style="background-color:' . $colour . '">';?>
+<?php echo '<body style="background-color:' . $colour . '">';//Displays Colour?>
 <body>
 	
     <h1>Membership Valdiation</h1>
@@ -79,10 +85,10 @@
         <input type="submit" id="button" value="Submit" onclick="">
     </form>
 	<h3>Student Number: <?php echo $cardString; ?></h3>
-	<h1 id="valid"><?php echo $valid; ?> </h1>
+	<h1 id="valid"><?php echo $valid; //Displays if valid or invalid?> </h1>
 
 	<?php 
-	if(strcmp($valid,'INVALID') == 0) {
+	if(strcmp($valid,'INVALID') == 0) {//If member is invalid this prints out the reason
 		if(strcmp($membership,'no') == 0){
 			echo '<h2>Membership not Purchased</h2>';
 		}
